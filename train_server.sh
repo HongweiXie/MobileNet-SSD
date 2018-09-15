e34adac7dc2cfd9745c4bed35c39c00cd9932d85
HongweiXie <hongwei.xhw@alibaba-inc.com>
2018-09-15 13:18:57 +0800

    add server train

---
+++ train_server.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if ! test -f example/MobileNetSSD_train.prototxt ;then
+	echo "error: example/MobileNetSSD_train.prototxt does not exist."
+	echo "please use the gen_model.sh to generate your own model."
+        exit 1
+fi
+export PYTHONPATH=../caffe/python
+#"mobilenet_iter_73000.caffemodel"
+snapshot_dir=diandu/snapshot_point_15
+mkdir -p ${snapshot_dir}
+latest=$(ls -t ${snapshot_dir}/*.caffemodel | head -n 1)
+echo $latest
+../caffe/build/tools/caffe train -solver="solver_train.prototxt" \
+-weights=$latest \
+-gpu 0,1,2,3,4,5,6,7
+
+
+
+
